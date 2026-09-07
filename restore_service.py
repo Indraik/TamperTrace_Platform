9805e22b30d4f85388fc2625d3b968ee6981eef9
V. Indra Kumar <indrakumarvelusamy@gmail.com>
2026-09-07 15:40:31 +0530

    chore: Remove legacy wrapper files and unused empty directories

--- restore_service.py
+++
@@ -1,7 +0,0 @@
-"""
-Legacy compatibility wrapper for restore_service.py.
-Domain implementation has been refactored into app.services.restore_service.
-"""
-from app.services.restore_service import RestoreService, restore_website
-
-__all__ = ["RestoreService", "restore_website"]
